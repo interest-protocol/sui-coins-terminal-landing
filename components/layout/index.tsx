b92be9582a54aa8511eef0c05a028acb71c0c260
Atila Aeseo Sousa <atilaesousa@gmail.com>
2024-10-16 10:34:36 +0100

    :bug: fix: layout styles

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -5,7 +5,10 @@ import Header from '../header';
 import Footer from './footer';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => (
-  <Main>
+  <Main
+    pt={['2rem', '2rem', '2rem', '3.5rem', '3.5rem']}
+    px={['2rem', '2rem', '2rem', '4.5rem', '4.5rem']}
+  >
     <Header />
     {children}
     <Footer />
