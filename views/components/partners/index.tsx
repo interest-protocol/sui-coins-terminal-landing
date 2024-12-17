64899bc994dde4265a2e4cd19e81f966782cf729
Mário Batalha <37873239+MarioBatalha@users.noreply.github.com>
2024-12-17 20:07:34 +0000

    :sparkles: feat: add partner section (#27)

--- views/components/partners/index.tsx
+++ views/components/partners/index.tsx
@@ -1,27 +1,67 @@
-import { Div } from '@stylin.js/elements';
+import { Box, Typography } from '@interest-protocol/ui-kit';
+import Link from 'next/link';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
-import { PARTNERS_DATA } from './partners.data';
+import { PARTNERS_IMAGES } from './partners.data';
 
 const Partners: FC = () => (
-  <Div pt="6rem" display="flex">
-    <Div
+  <Box px="xl">
+    <Box
+      py="8xl"
+      my="2rem"
+      gap="0.5rem"
       display="flex"
-      alignItems="center"
-      mx="auto"
-      gap={['2.5rem', '2.5rem', '3rem']}
-      flexDirection={['column', 'row']}
-      flexWrap="wrap"
+      maxWidth="1440px"
+      flexDirection="column"
+      alignItems="flex-start"
       justifyContent="center"
     >
-      {PARTNERS_DATA.map(({ size, Icon }) => (
-        <Div height="3rem" width={size} key={v4()}>
-          <Icon maxHeight="3rem" maxWidth={size} width="100%" />
-        </Div>
-      ))}
-    </Div>
-  </Div>
+      <Typography
+        pb="8xl"
+        size="large"
+        variant="title"
+        fontWeight="700"
+        color="#fff"
+        lineHeight="100%"
+        fontSize={['2.25rem', '2.25rem', '3.5rem']}
+      >
+        Our Partners
+      </Typography>
+      <Box
+        width="100%"
+        height="auto"
+        display="flex"
+        flexWrap="wrap"
+        alignItems="center"
+        justifyContent="center"
+        py={['m', 'm', 'm', '2xl']}
+        px={['m', 'm', 'm', '5rem']}
+      >
+        {PARTNERS_IMAGES.map((item) => (
+          <Box
+            m="1rem"
+            key={v4()}
+            width="auto"
+            cursor="pointer"
+            overflow="hidden"
+          >
+            <Link href={item.url} target="blank">
+              <img
+                src={item.image}
+                alt={item.label}
+                style={{
+                  width: '5rem',
+                  height: '5rem',
+                  objectFit: 'cover',
+                }}
+              />
+            </Link>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  </Box>
 );
 
 export default Partners;
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -3,11 +3,13 @@ import { FC } from 'react';
 import { Layout } from '@/components';
 
 import { HeroSection, TerminalSection } from '../components';
+import Partners from '../components/partners';
 
 const Home: FC = () => (
   <Layout>
     <HeroSection />
     <TerminalSection />
+    <Partners />
   </Layout>
 );
 
